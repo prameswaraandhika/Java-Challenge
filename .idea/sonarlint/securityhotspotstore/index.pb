978d84ad8ce0027562d2adc47ac68c24665c199a
Andhikap <prameswaara@gmail.com>
2023-09-17 09:52:33 +0700

    optimizing this code

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,11 @@
+
+V
+&src/main/java/org/example/Pesanan.java,9\1\91222cc1f2800b49a12f1e714e7ef6f407844fa2
+7
+pom.xml,4\4\442292b8a7efeabbe4cc176709b833b1792140ec
+Z
+*src/main/java/org/example/MenuMakanan.java,4\6\460a5399dae8c342c9704bc4127a6fdbe8a23c4a
+S
+#src/main/java/org/example/Main.java,0\b\0ba1b9c7a4878802f938e331cc2d8a042087ec6b
+F
+invoice_pembayaran.txt,3\7\374a762b037450233cb3dad86d3741dc13ab5a34
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,14 +10,16 @@ import java.util.Scanner;
 
 public class Main {
 //    List untuk menuMakanan
-    static List<MenuMakanan> menuMakanan = List.of(
+    static String line1 = "================================================================";
+    static String line2 = "---------------------------------------------------------------+\n";
+    static String formatPesanan = "%-14s %5d %15s\n";
+    static final List<MenuMakanan> menuMakanan = List.of(
                 new MenuMakanan(1,"Nasi Goreng", 15000),
-                new MenuMakanan(2, "Mie Goreng", 13000),
+                new MenuMakanan(2,"Mie Goreng", 13000),
                 new MenuMakanan(3,"Nasi + Ayam", 18000),
                 new MenuMakanan(4,"Es Teh Manis", 3000),
                 new MenuMakanan(5,"Es Jeruk", 5000));
-//    List untuk pesananMakanan
-    static List<Pesanan> menuPesanan = new ArrayList<Pesanan>();
+    static List<Pesanan> menuPesanan = new ArrayList<>();
 
     public static void main(String[] args) {
         menuUtamaTampilan();
@@ -26,11 +28,12 @@ public class Main {
     private static void menuUtamaTampilan() {
 
 //        Buat tampilan cli yang akan di ulang hingga selama user tidak memilih keluar dari aplikasi dan tidak menyelesaikan pembayaran
-        dance:
+//        karna switch 1 sd 5 adalah fitur memesan makanan, jadi saya buat satu scope
+//        ketika user memilih keluar dari aplikasi maka loop akan berhenti dan program akan keluar
         while (true) {
-            System.out.println("===========================");
+            System.out.println(line1);
             System.out.println("Selamat datang di BinarFud");
-            System.out.println("===========================");
+            System.out.println(line1);
             System.out.println("\nSilahkan pilih makanan :");
             for (MenuMakanan makanan : menuMakanan) {
                 System.out.printf("%d. %-20s | %s \n", makanan.id(), makanan.nama(), rupiahKonversi(makanan.harga()));
@@ -39,77 +42,69 @@ public class Main {
             System.out.println("0. Keluar aplikasi");
             System.out.print("\n=> ");
             byte pilihan = new Scanner(System.in).nextByte();
-//            karna switch 1 sd 5 adalah fitur memesan makanan, jadi saya buat satu scope
             switch (pilihan) {
-                case 1, 2, 3, 4, 5:
-                    konfirmasiTampilan(pilihan);
-                    break;
-                case 99:
-                    pembayaranTampilan();
-                    break;
-                case 0:
-//              ketika user memilih keluar dari aplikasi maka loop akan berhenti dan program akan keluar
-                    break dance;
+                case 1, 2, 3, 4, 5 -> konfirmasiTampilan(pilihan);
+                case 99 -> pembayaranTampilan();
+                case 0 ->
+                        System.exit(0);
+                default -> System.out.println("Input tidak dikenal");
             }
         }
     }
 
     private static void pembayaranTampilan() {
-        System.out.println("===========================");
+        System.out.println(line1);
         System.out.println("Konfirmasi & Pembayaran");
-        System.out.println("=========================================");
+        System.out.println(line1);
         int jumlahTotal = 0;
         long jumlahHarga = 0;
         for (Pesanan objek: menuPesanan) {
-            System.out.printf("%-14s %5d %15s\n", objek.nama(), objek.jumlah(), rupiahKonversi(objek.harga()));
+            System.out.printf(formatPesanan, objek.nama(), objek.jumlah(), rupiahKonversi(objek.harga()));
             jumlahTotal += objek.jumlah();
             jumlahHarga += objek.harga();
         }
-        System.out.println("---------------------------------------+");
-        System.out.printf("%-14s %5d %15s\n", "total", jumlahTotal, rupiahKonversi(jumlahHarga));
+        System.out.println(line2);
+        System.out.printf(formatPesanan, "total", jumlahTotal, rupiahKonversi(jumlahHarga));
         System.out.println("1. Konfirmasi dan Bayar");
         System.out.println("2. Kembali ke Menu Utama");
         System.out.println("0. Keluar Aplikasi");
         System.out.print("\n=> ");
         byte pilihan = new Scanner(System.in).nextByte();
         switch (pilihan) {
-            case 1 -> {
-                try {
-                    cetakInvoice(jumlahTotal, jumlahHarga);
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
-                System.exit(0);
-            }
-            case 3 -> System.exit(0);
+            case 1 -> cetakInvoice(jumlahTotal, jumlahHarga);
+            case 2 -> System.out.println("Kembali ke menu Utama");
+            case 0 -> System.exit(0);
+            default -> System.out.println("Input tidak dikenal");
         }
     }
 
-    private static void cetakInvoice(int jumlahTotal, long jumlahHarga) throws IOException {
-        BufferedWriter writer = new BufferedWriter(new FileWriter("invoice_pembayaran.txt"));
-        writer.write("=====================================");
-        writer.write("\nBinarFud\n");
-        writer.write("=====================================");
-        writer.write("\nTerima kasih sudah memesan\ndi BinarFud");
-        writer.write("\n\nDibawah ini pesanan anda\n\n");
-        for (Pesanan objek : menuPesanan) {
-            writer.write(String.format("%-14s %5d %15s\n", objek.nama(), objek.jumlah(), rupiahKonversi(objek.harga())));
+    private static void cetakInvoice(int jumlahTotal, long jumlahHarga) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("invoice_pembayaran.txt"))) {
+            writer.write(line1);
+            writer.write("\nBinarFud\n");
+            writer.write(line1);
+            writer.write("\nTerima kasih sudah memesan\ndi BinarFud");
+            writer.write("\n\nDibawah ini pesanan anda\n\n");
+            for (Pesanan objek : menuPesanan) {
+                writer.write(String.format("%-14s %5d %15s\n", objek.nama(), objek.jumlah(), rupiahKonversi(objek.harga())));
+            }
+            writer.write(line2);
+            writer.write(String.format("%-14s %5d %15s\n\n", "total", jumlahTotal, rupiahKonversi(jumlahHarga)));
+            writer.write("Pembayaran : Binar Cash\n");
+            writer.write(line1);
+            writer.write("\nSimpan struk ini sebagai \n");
+            writer.write("Bukti pembayaran\n");
+            writer.write(line1);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
-        writer.write("---------------------------------------------------------------+\n");
-        writer.write(String.format("%-14s %5d %15s\n\n", "total", jumlahTotal, rupiahKonversi(jumlahHarga)));
-        writer.write("Pembayaran : Binar Cash\n");
-        writer.write("================================================================\n");
-        writer.write("Simpan struk ini sebagai \n");
-        writer.write("Bukti pembayaran\n");
-        writer.write("=================================================================");
-        writer.close();
     }
 
     private static void konfirmasiTampilan(byte pilihan) {
-        System.out.println("===========================");
+        System.out.println(line1);
         System.out.println("Berapa pesanan anda");
-        System.out.println("===========================");
-        System.out.printf(" %s | %s \n", menuMakanan.get(pilihan-1).nama(), rupiahKonversi(menuMakanan.get(pilihan-1).harga()));
+        System.out.println(line1);
+        System.out.println(" " + menuMakanan.get(pilihan-1).nama() + "  |  " + menuMakanan.get(pilihan -1));
         System.out.println("Input 0 untuk kembali");
         System.out.print("\nqty => ");
         String nama = menuMakanan.get(pilihan - 1 ).nama();
@@ -129,11 +124,10 @@ public class Main {
         }
 //        lalu data akan ditambahkan ulang, untuk mencegah duplikasi menu pada listPesanan
         menuPesanan.add(new Pesanan(nama, jumlah, harga));
-        System.out.println(menuPesanan);
     }
 
     private static String rupiahKonversi(long harga){
-        DecimalFormat pattern = new DecimalFormat("#,###");
+        DecimalFormat pattern = new DecimalFormat("#.###");
         return pattern.format(harga);
     }
 
