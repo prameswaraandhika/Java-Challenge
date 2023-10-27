a55d4deb90486965f3b8a8d42069e61253d00fb7
Andhikap <prameswaara@gmail.com>
2023-10-27 16:58:06 +0700

    do something

--- src/main/java/org/example/challenge3/sql/InfoUser.java
+++
@@ -1,9 +0,0 @@
-package org.example.challenge3.sql;
-
-import java.util.UUID;
-
-public class InfoUser {
-	public final static UUID USER_ID = UUID.fromString("0acd0373-f682-4482-b797-826c8dfcf90a");
-	public final static String NAME = "dhikap";
-	public final static String DESTINATION = "Jakarta";
-}
