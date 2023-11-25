944bd170ce9ea0ae216cf08f5b0170717113c54c
Andhikap <prameswaara@gmail.com>
2023-11-25 13:47:30 +0700

    created endpoint for authenticated

--- challenge6/src/main/java/com/challenge6/app/controller/UserController.java
+++ challenge6/src/main/java/com/challenge6/app/controller/UserController.java
@@ -1,13 +1,19 @@
 package com.challenge6.app.controller;
 
+import com.challenge6.app.model.AuthRequestDto;
 import com.challenge6.app.model.User;
 import com.challenge6.app.model.dtos.UserNewDto;
 import com.challenge6.app.model.dtos.UserUpdatePasswordDto;
 import com.challenge6.app.service.AuthService;
+import com.challenge6.app.service.JwtService;
 import com.challenge6.app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
@@ -18,6 +24,8 @@ import java.util.UUID;
 @RequestMapping(value = {"/restapi/users"})
 public class UserController {
     final UserService userService;
+    final JwtService jwtService;
+    final AuthenticationManager authenticationManager;
 
     @PostMapping("/new")
     public ResponseEntity<?> addNewUser(@RequestBody User userInfo) {
@@ -37,8 +45,19 @@ public class UserController {
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> updatePasswordUser(@PathVariable UUID id){
+    public ResponseEntity<?> deletePasswordUser(@PathVariable UUID id){
         return userService.delete(id);
     }
 
+    @PostMapping("/authenticate")
+    public String authenticateAndGetToken(@RequestBody AuthRequestDto authRequest) {
+        Authentication authentication = authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        authRequest.getUsername(), authRequest.getPassword()));
+        if (authentication.isAuthenticated()) {
+            return jwtService.generateToken(authRequest.getUsername());
+        } else {
+            throw new UsernameNotFoundException("invalid user request!");
+        }
+    }
 }
